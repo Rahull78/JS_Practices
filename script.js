5d465c4f646897eaa7c4a28d792324465158bdd7
Rahull78 <101934952+Rahull78@users.noreply.github.com>
2024-10-01 18:52:44 +0530

    Completed Assignments

---
+++ script.js
@@ -0,0 +1,49 @@
+// let js = "amazing";
+// if (js === "amazing") {
+//   alert("JS is Fun");
+// }
+
+//number : always have floating point number either is decimal or intgear let=23
+//String : sequnce of character that always use for text and manuplation for text let js= 'deep'
+//Boolean : is a logical type is either sotre true or false its use for decesion making let age =true or false
+//undefind : varible declared but didnt asigned the values let = age;
+//null : varible declared but  asigned the values null let x = null;
+//symbol(Es2015) : defines the values uniqly
+
+//. Number
+
+let number = 30; //intgear number
+let floating = 30.5; //floting number
+
+console.log(number, floating); // this you can store multiple varible in one console
+
+//. String
+
+let string = "Hey!, my name is Rahul Modi";
+console.log(string);
+
+// Boolean
+
+let x = 10;
+let y = 20;
+
+if (x < y) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+let isRainyDay = true;
+console.log(isRainyDay);
+console.log(typeof isRainyDay);
+
+//Undefine
+
+var a;
+a = 20;
+console.log(typeof a);
+
+// null
+
+var aa = null;
+console.log(typeof aa);
