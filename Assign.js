0e44fcb05bf3e6a2cfa8bf5a4787438aabfd2e1e
Rahull78 <101934952+Rahull78@users.noreply.github.com>
2024-10-07 19:24:19 +0530

    Completed:Assignments & Challange

--- Assign.js
+++ Assign.js
@@ -137,3 +137,81 @@ if (numNeighbours === 1) {
 } else if (numNeighbours === 1) {
   console.log("no borders");
 }
+
+// /*1. Comment out the previous code so the prompt doesn't get in the way
+// 2. Let's say Sarah is looking for a new country to live in. She wants to live in a
+// country that speaks english, has less than 50 million people and is not an
+// island.
+// 3. Write an if statement to help Sarah figure out if your country is right for her.
+// You will need to write a condition that accounts for all of Sarah's criteria. Take
+// your time with this, and check part of the solution if necessary.
+// 4. If yours is the right country, log a string like this: 'You should live in Portugal :)'. If
+// not, log 'Portugal does not meet your criteria :('
+// 5. Probably your country does not meet all the criteria. So go back and temporarily
+// change some variables in order to make the condition true (unless you live in
+// Canada :D)*/
+
+let country4 = "Canada";
+const language2 = "English";
+let population4 = 40;
+const island = false;
+
+if (language2 === "English" && population4 < 50 && !island) {
+  console.log(`You should live in ${country4}:`);
+} else {
+  console.log(`${country4} does not meet your criteria`);
+}
+
+// /*Use a switch statement to log the following string for the given 'language':
+// chinese or mandarin: 'MOST number of native speakers!'
+// spanish: '2nd place in number of native speakers'
+// english: '3rd place'
+// hindi: 'Number 4'
+// arabic: '5th most spoken language'
+// for all other simply log 'Great language too :*/
+
+const prompt2 = require("prompt-sync")();
+
+var languages = prompt2("Pick a languages : ");
+
+while (true) {
+  switch (languages) {
+    case "chchinese":
+    case "mandarin":
+      console.log("MOST number of native speakers!");
+      break;
+
+    case "spanish":
+      console.log("2nd place in number of native speakers");
+      break;
+
+    case "english":
+      console.log("3rd place");
+      break;
+
+    case "arabic":
+      console.log("5th most spoken languag");
+      break;
+
+    case "english":
+      console.log("3rd place");
+      break;
+
+    default:
+      console.log("for all other simply log Great language too");
+  }
+}
+
+/*1. If your country's population is greater than 33 million, use the ternary operator 
+to log a string like this to the console: 'Portugal's population is above average'. 
+Otherwise, simply log 'Portugal's population is below average'. Notice how only
+one word changes between these two sentences!
+2. After checking the result, change the population temporarily to 13 and then to*/
+
+const country5 = "Portugal";
+console.log(country5);
+const populations =
+  32 >= 33
+    ? `${country5}'s population is above average`
+    : `${country5}'s population is below average`;
+console.log(country5, populations);
